30fb29edc977f9f6dc6856913404f446b5459c13
babakkhezry <babak.khezri7@gmail.com>
2020-02-16 20:52:42 +0330

    make some fixes

--- Typing.py
+++ Typing.py
@@ -3,26 +3,41 @@ from click import *
 from random import *
 from os import *
 def delete(word):
-    x = len(word)
+    x = (len(word))
+    if x == 0:
+        return ""
     lst = []
     for i in range(x):
         lst.append(word[i])
-    lst.remove(lst[x-1])
+    lst[x-1] = ""
     word = ""
     for i in lst:
         word += i
     return word
+print("Press Enter to start : ")
+while 1:
+    starter = getchar()
+    if ord(starter) == 13:
+        system('clear')
+        break
+    else:
+        for i in range(20):
+            system('clear')
+            sleep(0.001)
+            print("Press Enter to start : ")
 time_1 = time()
 time_2 = time()
 count = 0
-lst = ["home","book","door","think","monster","hello","good","self","night","sun","car","at","drive","phone","cost","allow","start","type","gold","green","sky","home","country","water","what","sleep","cake","freind","programing","level","today","search","queen","player","game","six","seven","movie","music"]
+lst = ["smoke","struct","coding","windows","pattern","special","terminal","sandwich","though","enough","might","printer","problem","computer","speak","perhaps","home","book","door","think","monster","hello","good","self","night","sun","car","at","drive","phone","cost","allow","start","type","gold","green","sky","home","country","water","what","sleep","cake","freind","programing","level","today","search","queen","player","game","six","seven","movie","music","map","while","sound","order","number"]
 numb = []
-len_words = len(lst)+1
+len_words = len(lst) - 1
 while (time_2 - time_1) < 60:
     n = randint(0,(len_words-1))
     if n not in numb:
         numb.append(n)
-        print(lst[n])
+        print(lst[n],end = "   |   your left time is : ")
+        print(int(60 -(time_2 - time_1)),end = "        your true answers : ")
+        print(count)
         word = ""
         ch = ''
         while 1:
@@ -32,14 +47,18 @@ while (time_2 - time_1) < 60:
                 break
             if ord(ch) == 127:
                 word = delete(word)
+                system('clear')
+                print(lst[n],end = "   |   your left time is : ")
+                print(int(60 -(time_2 - time_1)),end = "        your true answers : ")
+                print(count)
                 print(word)
-                
             else:
                 system('clear')
                 word += ch
-                print(lst[n])
+                print(lst[n],end = "   |   your left time is : ")
+                print(int(60 -(time_2 - time_1)),end = "        your true answers : ")
+                print(count)
                 print(word)
-
         if word in lst:
             count += 1
     time_2 = time()
@@ -49,5 +68,4 @@ if count <= 15 :
 if count > 15 and count <= 35:
     print("normal typing can be better")
 if count > 35:
-    print("well done you are perfect")
-    
+    print("well done you are perfect") 
