ddd00f0a20c5e531f80bdd95c1600d9a94767949
unknown <babak.khezri7@gamil.com>
2020-04-11 01:55:42 +0430

    Use Thread and some fixes

--- Typing.py
+++ Typing.py
@@ -1,35 +1,50 @@
-from time import time,sleep
+from time import sleep
 from click import getchar
 from random import randint
-from os import system
+from os import system, _exit
 from tqdm import tqdm
 import colorama as co
+from threading import Thread
 # to use back space
-def delete(word):
+trueAnswer = ture_ans = time = 0
+
+
+def timer(): # Timer for 1 minute
+    global time
+    time = 60
+    while time != 0:
+        sleep(1)
+        time -= 1
+    system('cls')
+    result(trueAnswer, ture_ans)
+
+
+def delete(word): # Word Backspace
     if len(word) == 0:
         return ""
-    #take apart the word
+    # take apart the word
     lst = list(word)
-    #delete the last letter
-    lst.remove(lst[-1])
-    # remake the word
-    word = "".join(lst)
-    return word
-#show the result of them game
-def result(trueAnswer,ture_ans):
-    print( co.Fore.BLUE + "your speed type is : ",format(trueAnswer - 1)," word in minute and your accuracy : %",end=format(int(ture_ans)))
-    #print(ture_ans)
-    if trueAnswer <= 15 :
-        print("\nyou are slow need practice")
+    # delete the last letter
+    lst.pop(-1)
+    return "".join(lst)
+
+
+def result(trueAnswer, ture_ans): # show the result of them game
+    print(co.Fore.BLUE + "your speed type is <{}> word in minute and your accuracy : %{}".format(trueAnswer, int(ture_ans)))
+    # print(ture_ans)
+    if trueAnswer <= 15:
+        print("you are slow need practice")
     if trueAnswer > 15 and trueAnswer <= 35:
-        print("\nnormal typing can be better")
+        print("normal typing can be better")
     if trueAnswer > 35:
-        print("\nwell done you are perfect") 
-# get acces to start program and start timer
-def starter():
-    print(co.Style.BRIGHT + co.Fore.LIGHTGREEN_EX +  " ")
+        print("well done you are perfect")
+    _exit(0)
+
+
+def starter():  # get acces to start program and start timer
+    print(co.Style.BRIGHT + co.Fore.LIGHTGREEN_EX + " ")
     co.Fore.LIGHTGREEN_EX
-    for i in tqdm(range(15),ncols=70):
+    for i in tqdm(range(15), ncols=70):
         sleep(0.1)
     system("cls")
     print(co.Fore.BLUE + "Press Enter to start : ")
@@ -37,6 +52,7 @@ def starter():
         starter = getchar()
         if ord(starter) == 13:
             system('cls')
+            Thread(target=timer).start()
             game()
         else:
             # just some thing for beauty
@@ -44,28 +60,35 @@ def starter():
                 system('cls')
                 sleep(0.001)
                 print(co.Fore.BLUE + "Press Enter to start : ")
-#main structer of game
-def game():
-    time_1 = time_2 = time()
+
+
+def game():  # main structer of game
+    global trueAnswer, ture_ans
     trueAnswer = fail = all_w = 0
-    lst = ["smoke","struct","coding","windows","forward","smoke","silver","soldier","stump","google","linux","pattern","special","mother","language","terminal","sandwich","though","enough","might","printer","problem","computer","speak","perhaps","home","book","door","think","monster","hello","good","self","night","sun","car","at","drive","phone","cost","allow","start","type","gold","green","sky","home","trust","water","what","sleep","cake","freind","programing","level","today","search","queen","player","game","six","seven","movie","music","map","while","sound","order","number"]
+    lst = ["smoke", "struct", "mobile", "coding", "windows",
+        "forward","smoke", "silver", "soldier", "stump", "google", 
+        "linux", "pattern", "special", "mother", "language", "terminal", 
+        "sandwich", "though", "enough", "might", "printer", "problem", "computer",
+        "speak", "perhaps", "home", "book", "door", "think", "monster", "hello","good",
+        "self", "night", "sun", "car", "at", "drive", "phone", "cost", "allow", "start",
+        "type", "gold", "green", "sky", "home", "trust", "water", "what", "sleep", "cake",
+         "freind", "programing", "level", "today", "search", "queen", "player", "game", "six",
+          "seven", "movie", "music", "map", "while", "sound", "order", "number"]
     numb = []
-    #get the number of words
-    len_words = len(lst) - 1
-    while (time_2 - time_1) < 60:
-        #random get one of the words
-        n = randint(0,(len_words-1))
+    while True:
+        # random get one of the words
+        n = randint(0, (len(lst) - 2))
         # dont let come repetitious words
         if n not in numb:
             numb.append(n)
-            print(co.Fore.GREEN + lst[n].ljust(10),end = co.Fore.YELLOW + "|\tyour left time is : ")
-            time_2 = time()
-            print(int(60 -(time_2 - time_1)), co.Fore.RED + "\t\tyour true answers : {}".format(trueAnswer))
+            print(co.Fore.GREEN + lst[n].ljust(10),
+                  end=co.Fore.YELLOW + "|\tyour left time is : ")
+            print(time, co.Fore.RED +
+                  "\t\tyour true answers : {}".format(trueAnswer))
             word = ""
-            ch = ''
             while True:
                 ch = getchar()
-                #if put space end the procec
+                # if put space end the procec
                 if ch == " ":
                     system('cls')
                     break
@@ -73,27 +96,29 @@ def game():
                 if ord(ch) == ord("\b"):
                     word = delete(word)
                     system('cls')
-                    print(co.Fore.GREEN + lst[n].ljust(10),end = co.Fore.YELLOW + "|\tyour left time is : ")
-                    time_2 = time()
-                    print(int(60 -(time_2 - time_1)),end = co.Fore.RED + "\t\tyour true answers : {}\n".format(trueAnswer))
-                    print(co.Fore.GREEN +  word)
+                    print(
+                        co.Fore.GREEN + lst[n].ljust(10), end=co.Fore.YELLOW + "|\tyour left time is : ")
+                    print(time, end=co.Fore.RED +
+                          "\t\tyour true answers : {}\n".format(trueAnswer))
+                    print(co.Fore.GREEN + word)
                 else:
                     system('cls')
                     word += ch
-                    print(co.Fore.GREEN + lst[n].ljust(10),end = co.Fore.YELLOW + "|\tyour left time is : ")
-                    time_2 = time()
-                    print(int(60 -(time_2 - time_1)),end = co.Fore.RED + "\t\tyour true answers : {}\n".format(trueAnswer))
-                    print(co.Fore.GREEN +  word)
-            #trueAnswer all words that come
+                    print(
+                        co.Fore.GREEN + lst[n].ljust(10), end=co.Fore.YELLOW + "|\tyour left time is : ")
+                    print(time, end=co.Fore.RED +
+                          "\t\tyour true answers : {}\n".format(trueAnswer))
+                    print(co.Fore.GREEN + word)
+            # trueAnswer all words that come
             all_w += 1
-            #final check
+            # final check
             if word == lst[n]:
                 trueAnswer += 1
             else:
-                #count all wrong words
+                # count all wrong words
                 fail += 1
-        time_2 = time()
-    #find the porsant of true typing
-    ture_ans = 100 - ((100 * fail) / all_w)
-    result(trueAnswer,ture_ans)
+            # find the porsant of true typing
+            ture_ans = 100 - ((100 * fail) / all_w)
+
+
 starter()
